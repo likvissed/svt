193b82ce1a6389e8dbd893850161e2515b800bd9
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-04-27 16:35:46 +0700

    Added tn_uniqueness_per_workplace_count validation. Changed locale file.

--- app/models/inventory/workplace_responsible.rb
+++ app/models/inventory/workplace_responsible.rb
@@ -6,6 +6,16 @@ module Inventory
     belongs_to :workplace_count, inverse_of: :workplace_responsibles
     belongs_to :user_iss, foreign_key: 'id_tn', optional: true
 
+    validate :tn_uniqueness_per_workplace_count
+
     attr_accessor :tn
+
+    private
+
+    # Валидация, проверяющая уникальность табельного номера в рамках текущего workplace_count.
+    def tn_uniqueness_per_workplace_count
+      errors.add(:tn, "'#{self.tn}' уже существует") if self.class.exists?(workplace_count: workplace_count, id_tn:
+        id_tn, phone: phone)
+    end
   end
 end
