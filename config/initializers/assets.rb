3a7e31f256bf84eb59efc8e2285af2171f7c8747
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-07-12 10:35:40 +0700

    Add bootstrap fonts manually

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,7 +7,7 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << "#{Rails}/vendor/assets/fonts"
+# Rails.application.config.assets.paths << "#{Rails}/vendor/assets/fonts"
 
 # Precompile additional assets.
 # application.js, application.sass, and all non-JS/CSS in app/assets folder are already added.
