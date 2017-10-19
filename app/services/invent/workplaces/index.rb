c1604126b5ac373f8d7533db9832df085e729575
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-10-19 10:05:41 +0700

    Optimized SQL query in the Workplace::Index service.

--- app/services/invent/workplaces/index.rb
+++ app/services/invent/workplaces/index.rb
@@ -26,9 +26,8 @@ module Invent
 
       def load_workplace
         @workplaces = Workplace
-                        .left_outer_joins(:workplace_type, :inv_items, :user_iss)
-                        .select('invent_workplace.*, invent_workplace_type.short_description as wp_type,
- count(invent_item.item_id) as count')
+                        .left_outer_joins(:workplace_type, :user_iss)
+                        .select('invent_workplace.*, invent_workplace_type.short_description as wp_type')
                         .where('fio LIKE ? OR fio is NULL', "%#{@search[:value]}%")
                         .group(:workplace_id)
       end
@@ -53,19 +52,21 @@ module Invent
       def limit_records
         @data[:recordsFiltered] = @workplaces.length
         @workplaces = @workplaces
-                        .includes(:iss_reference_site, :iss_reference_building, :iss_reference_room, :user_iss, :workplace_count)
+                        .includes(%i[inv_items iss_reference_site iss_reference_building iss_reference_room user_iss workplace_count])
                         .limit(@length).offset(@start)
       end
 
       def prepare_to_render
         @data[:data] = @workplaces.as_json(
-          include: %i[iss_reference_site iss_reference_building iss_reference_room user_iss workplace_count]
+          include: %i[inv_items iss_reference_site iss_reference_building iss_reference_room user_iss workplace_count]
         ).each do |wp|
           wp['location'] = wp_location_string(wp)
           wp['responsible'] = wp['user_iss'] ? wp['user_iss']['fio'] : 'Ответственный не найден'
           wp['status'] = Workplace.translate_enum(:status, wp['status'])
           wp['division'] = wp['workplace_count']['division']
+          wp['count'] = wp['inv_items'].count
 
+          wp.delete('inv_items')
           wp.delete('workplace_count')
           wp.delete('iss_reference_site')
           wp.delete('iss_reference_building')
