399968208870979a529f68f095e8a37fafd58e88
Bayburin R.F <bayburin@iss-reshetnev.ru>
2017-11-13 15:51:36 +0700

    Refactoring 'items...' javascript files with ES6

--- app/assets/javascripts/invent/items/items_controller.js
+++ app/assets/javascripts/invent/items/items_controller.js
@@ -1,60 +1,67 @@
 (function () {
   'use strict';
 
+  class InventItemsCtrl {
+    constructor(InvItem) {
+      this._InvItem = InvItem;
+
+      this._loadItems(true);
+    }
+
+    /**
+     * Загрузить логи с сервера.
+     */
+    _loadItems(initFlag) {
+      this._InvItem.init(initFlag).$promise.then(() => this.items = this._InvItem.items)
+    }
+
+    get filters() {
+      return this._InvItem.filters.selected;
+    }
+
+    get lists() {
+      return this._InvItem.filters.lists;
+    }
+
+    get pagination() {
+      return this._InvItem.pagination;
+    }
+
+    /**
+     * События изменения номера страницы.
+     */
+    changePage() {
+      this._loadItems(false);
+    }
+
+    /**
+     * Событие изменения фильтра.
+     */
+    changeFilter() {
+      this._loadItems(false);
+    }
+
+    /**
+     * Добавить фильтр по составу техники.
+     */
+    addPropFilter() {
+      this._InvItem.addPropFilter();
+    }
+
+    /**
+     * Удалить выбранный фильтр по составу техники
+     *
+     * @param index - индекс удаляемого элемента.
+     */
+    delPropFilter(index) {
+      if (this.filters.properties.length > 1) {
+        this._InvItem.delPropFilter(index);
+        this._loadItems(false);
+      }
+    }
+  }
+
   app.controller('InventItemsCtrl', InventItemsCtrl);
 
   InventItemsCtrl.$inject = ['InvItem'];
-
-  function InventItemsCtrl(InvItem) {
-    this.InvItem = InvItem;
-    this.pagination = InvItem.pagination;
-    this.filters = InvItem.filters.selected;
-    this.lists = InvItem.filters.lists;
-
-    this._loadItems(true);
-  }
-
-  /**
-   * Загрузить логи с сервера.
-   */
-  InventItemsCtrl.prototype._loadItems = function (initFlag) {
-    var self = this;
-
-    this.InvItem.init(initFlag).$promise.then(function () {
-      self.items = self.InvItem.items;
-    });
-  };
-
-  /**
-   * События изменения страницы.
-   */
-  InventItemsCtrl.prototype.changePage = function () {
-    this._loadItems(false);
-  };
-
-  /**
-   * Событие изменения фильтра.
-   */
-  InventItemsCtrl.prototype.changeFilter = function () {
-    this._loadItems(false);
-  };
-
-  /**
-   * Добавить фильтр по составу техники.
-   */
-  InventItemsCtrl.prototype.addPropFilter = function () {
-    this.InvItem.addPropFilter();
-  };
-
-  /**
-   * Удалить выбранный фильтр по составу техники
-   *
-   * @param index - индекс удаляемого элемента.
-   */
-  InventItemsCtrl.prototype.delPropFilter = function (index) {
-    if (this.filters.properties.length > 1) {
-      this.InvItem.delPropFilter(index);
-      this._loadItems(false);
-    }
-  };
 })();
