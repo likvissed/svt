58f4b776880c9f3efe9eed742ea2ed182c48f6cf
Bayburin R.F <bayburin@iss-reshetnev.ru>
2018-01-12 11:36:04 +0700

    Add Warehouse::ItemToOrder#uniq_inv_item_for_processing_order validation

--- app/models/warehouse/item_to_order.rb
+++ app/models/warehouse/item_to_order.rb
@@ -5,5 +5,23 @@ module Warehouse
 
     belongs_to :inv_item, class_name: 'Invent::Item', foreign_key: 'invent_item_id'
     belongs_to :order
+
+    validate :uniq_inv_item_for_processing_order
+
+    protected
+
+    def uniq_inv_item_for_processing_order
+      order = Order.includes(:item_to_orders).where(status: :processing)
+                .find { |o| o.item_to_orders.any? { |io| io.invent_item_id == invent_item_id } }
+      return unless order
+
+      errors.add(
+        :inv_item,
+        :uniq_by_processing_order,
+        type: inv_item.type.short_description,
+        invent_num: inv_item.invent_num,
+        order_id: order.warehouse_order_id
+      )
+    end
   end
 end
--- spec/models/warehouse/item_to_order_spec.rb
+++ spec/models/warehouse/item_to_order_spec.rb
@@ -4,5 +4,31 @@ module Warehouse
   RSpec.describe ItemToOrder, type: :model do
     it { is_expected.to belong_to(:inv_item).class_name('Invent::Item').with_foreign_key('invent_item_id') }
     it { is_expected.to belong_to(:order) }
+
+    describe '#uniq_inv_item_for_processing_order' do
+      let(:workplace) { create(:workplace_pk, :add_items, items: %i[pc monitor monitor]) }
+      let(:item) { workplace.items.first }
+      subject { build(:item_to_order, inv_item: item) }
+
+      context 'when order exists' do
+        let(:operations) { [build(:order_operation, invent_item_id: item.item_id)] }
+        let(:item_to_orders) { [build(:item_to_order, inv_item: item)] }
+        let!(:order) { create(:order, consumer_dept: item.workplace.workplace_count.division, operations: operations, item_to_orders: item_to_orders) }
+
+        it 'adds :uniq_by_processing_order error' do
+          subject.valid?
+          expect(subject.errors.details[:inv_item]).to include(
+            error: :uniq_by_processing_order,
+            type: item.type.short_description,
+            invent_num: item.invent_num,
+            order_id: order.warehouse_order_id
+          )
+        end
+      end
+
+      context 'when order is not exist' do
+        it { is_expected.to be_valid }
+      end
+    end
   end
 end
