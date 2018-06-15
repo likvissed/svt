2210866b857204a25d8f29fc8a691840d0bf6c89
Bayburin R.F <bayburin@iss-reshetnev.ru>
2018-06-15 09:39:36 +0700

    Fix N+1 request in list_wp page (v3)

--- app/services/invent/workplaces/base_service.rb
+++ app/services/invent/workplaces/base_service.rb
@@ -59,7 +59,10 @@ module Invent
       # Отфильтровать полученные данные
       def run_filters
         @workplaces = @workplaces.left_outer_joins(:user_iss).where('fio LIKE ?', "%#{@conditions['fullname']}%") if @conditions['fullname'].present?
-        @workplaces = @workplaces.includes(:items).where('invent_item.invent_num LIKE ?', "%#{@conditions['invent_num']}%").references(:items) if @conditions['invent_num'].present?
+        if @conditions['invent_num'].present?
+          items = Invent::Item.where('invent_item.invent_num LIKE ?', "%#{@conditions['invent_num']}%")
+          @workplaces = @workplaces.where(items: items)
+        end
         @workplaces = @workplaces.where(workplace_count_id: @conditions['workplace_count_id']) unless @conditions['workplace_count_id'].to_i.zero?
         @workplaces = @workplaces.where(status: @conditions['status']) if @conditions.has_key?('status') && @conditions['status'] != 'all'
         @workplaces = @workplaces.where(workplace_type_id: @conditions['workplace_type_id']) unless @conditions['workplace_type_id'].to_i.zero?
