7f81c5f1dcb8e672ebedc5a3142a5b55ba096be5
Bayburin R.F <bayburin@iss-reshetnev.ru>
2018-06-21 10:56:30 +0700

    Fix workplace_worker

--- app/workers/invent/workplace_worker.rb
+++ app/workers/invent/workplace_worker.rb
@@ -3,7 +3,7 @@ class Invent::WorkplaceWorker
 
   def perform(*args)
     ids = Invent::Workplace.where(status: :confirmed).includes(:user_iss, :workplace_count, :items).find_each.map do |wp|
-      next if wp.user_iss && wp.user_iss.dept == wp.division.to_i && wp.items.size.positive?
+      next if wp.user_iss && wp.user_iss.dept.to_i == wp.division.to_i && wp.items.size.positive?
 
       wp.workplace_id
     end.compact
