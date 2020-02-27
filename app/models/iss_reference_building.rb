aa56ce952cc9492a09848061139736bd9344b5ae
Lika <bartuzanovaan@iss-reshetnev.ru>
2020-02-27 08:34:13 +0700

    Добавила связи с таблицей Warehouse::Location

--- app/models/iss_reference_building.rb
+++ app/models/iss_reference_building.rb
@@ -4,6 +4,7 @@ class IssReferenceBuilding < Netadmin
   has_many :workplaces, class_name: 'Invent::Workplace', foreign_key: 'location_building_id'
   has_many :workplace_counts, through: :workplaces, class_name: 'Invent::WorkplaceCount'
   has_many :iss_reference_rooms, foreign_key: 'building_id'
+  has_many :locations, class_name: 'Warehouse::Location', foreign_key: 'building_id'
 
   belongs_to :iss_reference_site, foreign_key: 'site_id'
 end
