3695411b0d26cb26dada7af21fd2904cf7da39af
Lika <bartuzanovaan@iss-reshetnev.ru>
2020-12-23 15:15:07 +0700

    Добавление разрешенных ресурсов для получения api

--- config/application.rb
+++ config/application.rb
@@ -50,6 +50,8 @@ module Inv
         resource '/invent/***REMOVED***_invents/get_pc_script*', headers: :any, methods: [:get]
         resource '/invent/***REMOVED***_invents/existing_item*', headers: :any, methods: [:get]
         resource '/invent/***REMOVED***_invents/invent_item*', headers: :any, methods: [:get]
+        resource '/user_isses*', headers: :any,  methods: [:get]
+        resource '/api/v1/invent/items*', headers: :any,  methods: [:get]
       end
     end
 
