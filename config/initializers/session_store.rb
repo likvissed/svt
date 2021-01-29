abdfffb637a498352896ae8e39f5e289a7ad5dbf
Байбурин Равиль <bayburin@iss-reshetnev.ru>
2021-01-29 16:39:27 +0700

    Подключен docker.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.session_store :redis_store, {
   servers: [
-    { host: '127.0.0.1', port: 6379, db: 2 },
+    { host: ENV['REDIS_HOST'], port: 6379, db: 2 },
   ],
   key: '_inv_session'
 }
