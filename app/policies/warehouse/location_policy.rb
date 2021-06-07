bec41b1888bc7743b696c08b9c7150187314ab28
Lika <bartuzanovaan@iss-reshetnev.ru>
2021-06-07 13:20:21 +0700

    Разрешен доступ на расположение для ответственного за ВТ

--- app/policies/warehouse/location_policy.rb
+++ app/policies/warehouse/location_policy.rb
@@ -1,7 +1,12 @@
 module Warehouse
   class LocationPolicy < Warehouse::ApplicationPolicy
     def ctrl_access?
-      not_for_***REMOVED***_user
+      # Для фильтров корпусов и комнат на странице РМ
+      if user.role? :***REMOVED***_user
+        user.workplace_counts.any?
+      else
+        true
+      end
     end
   end
 end
